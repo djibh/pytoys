9633de0d191be6fa8eae426dc2ec8f4983d2185d
Djibh <julien.riera@protonmail.com>
2022-12-27 17:32:03 +0100

    (account view) refactoring

--- website/views.py
+++ website/views.py
@@ -20,23 +20,8 @@ def allowed_file(filename):
     return '.' in filename and \
         filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
-@views.route('/account', methods=['POST', 'GET'])
-def upload_file():
-    if request.method == 'POST':
-        product_title = request.form.get('product-title')
-        product_description = request.form.get('product-description')
-        product_category = request.form.get('product-category')
-        product_city = request.form.get('product-city')
-
-        if len(product_title) < 1:
-            flash('Product\'s description is too short!', category='error')
-        else:
-            new_product = Product(title=product_title, description=product_description, city=product_city, category=product_category, user_id=current_user.id)
-            db.session.add(new_product)
-            db.session.commit()
-            flash('Product added!', category='success')
-
-        # check if the post request has the file part
+def file_validation():
+    # check if the post request has the file part
         if 'file' not in request.files:
             flash('No file part')
             return redirect(request.url)
@@ -53,6 +38,24 @@ def upload_file():
             filename = secure_filename(file.filename)
             file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
             return redirect(url_for('views.home', name=filename))
+
+
+@views.route('/account', methods=['POST', 'GET'])
+def upload_file():
+    if request.method == 'POST':
+        product_title = request.form.get('product-title')
+        product_description = request.form.get('product-description')
+        product_category = request.form.get('product-category')
+        product_city = request.form.get('product-city')
+
+        if len(product_title) < 1:
+            flash('Product\'s description is too short!', category='error')
+        else:
+            new_product = Product(title=product_title, description=product_description, city=product_city, category=product_category, user_id=current_user.id)
+            file_validation()
+            db.session.add(new_product)
+            db.session.commit()
+            flash('Product added!', category='success')
 
     return render_template("account.html", user=current_user)
 
